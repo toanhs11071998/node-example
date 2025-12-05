b3465d280cf01e8d8dda525d64f9efa68542483e
toanhs <toanhs@f88.vn>
2025-12-05 15:02:01 +0700

    Add Vercel catch-all serverless function to handle /api/*

---
+++ api/[...all].js
@@ -0,0 +1,20 @@
+const serverless = require('serverless-http');
+require('dotenv').config();
+const connectDB = require('../config/database');
+const app = require('../index');
+
+// Wrap app once
+const handler = serverless(app);
+
+module.exports = async (req, res) => {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.error('DB connection error', err);
+    res.statusCode = 500;
+    res.end(JSON.stringify({ success: false, message: 'Database connection error' }));
+    return;
+  }
+
+  return handler(req, res);
+};
